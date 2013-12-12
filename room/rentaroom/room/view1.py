00b2fedc53c00e4815de3d34f770e4859d34eda1
edwinwow <yunlong.he@hotmail.com>
2013-12-12 10:19:40 +0800

    Update view1.py

--- room/rentaroom/room/view1.py
+++ room/rentaroom/room/view1.py
@@ -4,3 +4,36 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from .models import RoomForRent
 from .serializers import RoomForRentSerializer
+
+class JSONResponse(HttpResponse):
+  
+  """
+  An HttpResponse that renders its content into JSON.
+  """
+  
+  def __init__(self, data, **kwargs):
+    content = JSONRenderer().render(data)
+    kwargs['content_type'] = 'application/json'
+    super(JSONResponse, self).__init__(content, **kwargs)
+    
+
+def room_for_rent_list(request):
+  """
+  list all the rooms for rent
+  """
+  
+  if request.method == 'GET':
+    room_for_rent = RoomForRent.objects.all
+    serializer = RoomForRentSerializer(room_for_rent, many=True)
+    return JSONResponse(serializer.data)
+    
+  elif request.method == 'POST':
+    data = JSONParser().parse(request)
+    serializer = RoomForRentSerializer(data=data)
+    if serializer.is_valid():
+      serializer.save()
+      return JSONResponse(serializer.data, status=201)
+    else:
+      return JSONResponse(serializer.errors, status=400)
+    
+    
