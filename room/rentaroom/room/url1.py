28224dbd7e27e0092a74e554bb2cac7e043571ae
edwinwow <yunlong.he@hotmail.com>
2013-12-12 10:33:05 +0800

    Update url1.py

--- room/rentaroom/room/url1.py
+++ room/rentaroom/room/url1.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 from .view1 import room_for_rent_list, room_for_rent_detail
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('',
     url(r'^room_for_rent/$', 'room_for_rent_list'),
@@ -7,3 +8,4 @@ urlpatterns = patterns('',
 )
 
 
+urlpatterns = format_suffix_patterns(urlpatterns)
