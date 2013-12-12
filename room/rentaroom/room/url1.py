b2ec2c32b354df2201cf8ec2c58ebb55406addad
edwinwow <yunlong.he@hotmail.com>
2013-12-12 10:27:29 +0800

    Update url1.py

--- room/rentaroom/room/url1.py
+++ room/rentaroom/room/url1.py
@@ -1,1 +1,9 @@
 from django.conf.urls import patterns, url
+from .view1 import room_for_rent_list, room_for_rent_detail
+
+urlpatterns = patterns('',
+    url(r'^room_for_rent/$', 'room_for_rent_list'),
+    url(r'^room_for_rent/(?P<pk>[0-9]+)/$', 'room_for_rent_detail'),
+)
+
+
