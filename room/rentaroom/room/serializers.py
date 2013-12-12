9435bcb7ebab3021d9746eb6e5719131b7c52be8
edwinwow <yunlong.he@hotmail.com>
2013-12-12 21:23:10 +0800

    Update serializers.py

--- room/rentaroom/room/serializers.py
+++ room/rentaroom/room/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from .models import RoomForRent, 
 
 class RoomForRentSerializer(serializers.ModelSerializer):
+  room_for_rent = serializers.PrimaryKeyRelatedField(many=True)
   
   class Meta:
     model = RoomForRent
