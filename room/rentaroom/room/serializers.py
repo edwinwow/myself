f2a469681d453fdb731b5ab3f772a4706a101e3a
edwinwow <yunlong.he@hotmail.com>
2013-12-12 10:11:09 +0800

    Update serializers.py

--- room/rentaroom/room/serializers.py
+++ room/rentaroom/room/serializers.py
@@ -1,3 +1,13 @@
 from django.forms import widgets
 from rest_framework import serializers
 from .models import RoomForRent, 
+
+class RoomForRentSerializer(serializers.ModelSerializer):
+  
+  class Meta:
+    model = RoomForRent
+    fields = ('room_status', 'start_date', 'price', 'rating', 'comments')
+    
+    
+    
+
